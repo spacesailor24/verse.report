bf9aa7da4a7cc184f694401ece1941917e7fa283
Wyatt Barnes <me@wyatt.email>
2025-09-16 21:00:29 -1000

    chore: timeline responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <Timeline />
         <main className="flex-1 p-4">
           {/* Main content area */}
